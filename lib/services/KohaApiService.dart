05f944820df2449d833369aad3201fb240e3228b
ST10268917 <ST10268917@vcconnect.edu.za>
2025-07-23 09:39:38 +0200

    Initial commit

--- lib/services/KohaApiService.dart
+++ lib/services/KohaApiService.dart
@@ -5,20 +5,19 @@ import 'package:alburhaan/models/BookDetail.dart';
 
 class KohaApiService {
   final String baseUrl = "https://library.al-burhaan.org/api/v1/";
-  final String username = 'Alburhaan';
-  final String password = 'H1b1scus_16';
 
+  // Fetch a list of books (public access)
   Future<List<BookResponse>> fetchBooks(int page, {String? query}) async {
-    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$username:$password'));
     String url = "${baseUrl}biblios?_page=$page&_per_page=10";
+
     if (query != null && query.isNotEmpty) {
-      var queryJson = jsonEncode({"title": {"-like": "%$query%"}});
+      var queryJson = jsonEncode({
+        "title": {"-like": "%$query%"}
+      });
       url += "&q=${Uri.encodeComponent(queryJson)}";
     }
-    var response = await http.get(Uri.parse(url), headers: {
-      'Authorization': basicAuth,
-      'Accept': 'application/json; charset=utf-8'
-    });
+
+    var response = await http.get(Uri.parse(url));
 
     print("Requesting URL: $url");
     if (response.statusCode == 200) {
@@ -27,25 +26,25 @@ class KohaApiService {
       List<dynamic> booksJson = jsonDecode(responseBody);
       return booksJson.map((data) => BookResponse.fromJson(data)).toList();
     } else {
-      print("Failed to fetch books. Status code: ${response.statusCode}, Response: ${response.body}");
-      throw Exception('Failed to load books. Status code: ${response.statusCode}');
+      print(
+          "Failed to fetch books. Status code: ${response.statusCode}, Response: ${response.body}");
+      throw Exception(
+          'Failed to load books. Status code: ${response.statusCode}');
     }
   }
 
+  // Fetch detailed info for a specific book (public access)
   Future<BookDetail> fetchBookDetail(int biblioId) async {
-    String basicAuth = 'Basic ' + base64Encode(utf8.encode('$username:$password'));
     String url = "$baseUrl/biblios/$biblioId";
 
-    var response = await http.get(Uri.parse(url), headers: {
-      'Authorization': basicAuth,
-      'Accept': 'application/json; charset=utf-8'
-    });
+    var response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       var decodedData = utf8.decode(response.bodyBytes);
       return BookDetail.fromJson(json.decode(decodedData));
     } else {
-      throw Exception('Failed to load book detail. Status code: ${response.statusCode}');
+      throw Exception(
+          'Failed to load book detail. Status code: ${response.statusCode}');
     }
   }
 }
